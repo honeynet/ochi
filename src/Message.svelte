ae940c6ab8b0a632de127f09d137d140caa9286c
Lukas Rist <glaslos@gmail.com>
2022-05-23 23:49:02 +0200

    remove invalid import

--- src/Message.svelte
+++ src/Message.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { createEventDispatcher, onMount } from 'svelte';
-import { element } from 'svelte/internal';
-
+    
 	const dispatch = createEventDispatcher();
     
     export let message:messageType;
