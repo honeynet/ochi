a1bb01635b935dece0e56ac396390d977e998d7b
Lukas Rist <glaslos@gmail.com>
2022-05-28 12:23:44 +0200

    filter by port added

--- src/App.svelte
+++ src/App.svelte
@@ -7,15 +7,25 @@
 
 	export let messages: messageType[] = [];
 
-	let content: messageType;
-
 	$: if (messages.length >= 50) {
 		messages = messages.slice(1);
 	}
 
-	function addMessage(obj: messageType) {
-		messages.push(obj);
-		messages = messages;
+	let content: messageType;
+	let filter: string;
+
+	let filterPorts: number[] = [];
+
+	function addMessage(message: messageType) {
+		if (message.dstPort === null || !filterPorts.includes(message.dstPort)) {
+			messages.push(message);
+			messages = messages;
+		}
+	}
+
+	function filterMessages() {
+		filterPorts = filter.split("&&").map(Number);
+		messages = messages.filter(message => !filterPorts.includes(message.dstPort));
 	}
 
 	function dial() {
@@ -45,7 +55,7 @@
 		content = event.detail;
 	}
 
-	const sleep = (ms) => new Promise((f) => setTimeout(f, ms));
+	const sleep = (ms: number) => new Promise((f) => setTimeout(f, ms));
 
 	const test = async () => {
 		while (true) {
@@ -72,14 +82,20 @@
 </script>
 
 <header class="site-header">
-	<b>Ochi</b>: find me on <a href="https://github.com/glaslos/ochi">github/glaslos/ochi</a>
+	<b>Ochi</b>: find me at
+	<a target="_blank" href="https://github.com/glaslos/ochi">github/glaslos/ochi</a>
+	<input bind:value={filter} placeholder="Filter destination port" />
+	<button on:click={filterMessages}>Apply</button>
+	<span>Port number and '&&' to concat.</span>
 </header>
 
 <main>
 	<div class="row">
 		<div class="column" id="message-log">
 			{#each messages as message (message.timestamp)}
-				<Message on:message={displayContent} {message} />
+				{#if !filterPorts.includes(message.dstPort)}
+					<Message on:message={displayContent} {message} />
+				{/if}
 			{/each}
 		</div>
 		<Content {content} />
@@ -89,7 +105,7 @@
 <style>
 	.site-header {
 		border-bottom-style: solid;
-    	border-width: 1px;
+		border-width: 1px;
 	}
 
 	main {
@@ -100,7 +116,7 @@
 	.row {
 		display: flex;
 		position: absolute;
-		top: 30px;
+		top: 55px;
 		left: 0;
 		bottom: 0;
 		right: 0;
