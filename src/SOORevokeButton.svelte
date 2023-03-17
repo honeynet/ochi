2f559e27721dee658abe735eb5253c0132c64896
Aditya Garg <uber6707@gmail.com>
2023-03-17 12:40:18 +0530

    added prettier

--- src/SOORevokeButton.svelte
+++ src/SOORevokeButton.svelte
@@ -1,31 +1,28 @@
 <script lang="ts">
-	import { user } from "./store";
-	import { logout } from "./session";
-	import "google.accounts";
+    import { user } from './store';
+    import { logout } from './session';
+    import 'google.accounts';
 
-	let email: string;
-	user.subscribe((value) => {
-		email = value["email"];
-	});
+    let email: string;
+    user.subscribe((value) => {
+        email = value['email'];
+    });
 
-	function revokeSSO() {
-		google.accounts.id.revoke(
-			email,
-			(response: google.accounts.id.RevocationResponse) => {
-				if (response.successful) {
-					logout();
-				} else {
-					console.log(response.error);
-				}
-			}
-		);
-	}
+    function revokeSSO() {
+        google.accounts.id.revoke(email, (response: google.accounts.id.RevocationResponse) => {
+            if (response.successful) {
+                logout();
+            } else {
+                console.log(response.error);
+            }
+        });
+    }
 </script>
 
 <button id="revokeButton" on:click={revokeSSO}>Revoke</button>
 
 <style>
-	#revokeButton {
-		float: right;
-	}
+    #revokeButton {
+        float: right;
+    }
 </style>
