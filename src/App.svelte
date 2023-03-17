3f6292b0d01a7a125a991f158e4a9f5cfdd93487
Aditya Garg <uber6707@gmail.com>
2023-03-17 10:55:10 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -10,7 +10,6 @@
 	import { validate } from "./session";
 
 	import { isAuthenticated } from "./store";
-
 	// subscribe to the authentication status
 	let isLoggedIn: boolean;
 	isAuthenticated.subscribe((status) => {
@@ -19,18 +18,54 @@
 
 	export let messages: messageType[] = [];
 
+	let noOfMessages: number;
+	let content: messageType;
+	let filter: string;
+	let isDevOn: boolean;
+	let conn: WebSocket;
+
 	// truncate the number of messages show in the app
-	$: if (messages.length >= 50) {
+	$: if (messages.length > noOfMessages) {
 		messages = messages.slice(1);
 	}
 
-	let content: messageType;
-	let filter: string;
-
+	$: if(isDevOn){
+		test();
+	} else{
+		dial(conn);
+	}
+	
 	let filterPorts: number[] = [];
 
-	//   To set auto follow work or not
-	let follow: boolean = true;
+	function defineMessages (event:any) {
+		let chosenValue = event.target.value;
+		let presentMessages = messages.length;
+
+		if(chosenValue<=0){
+			return;
+		}
+
+		if(chosenValue < presentMessages){
+			messages = messages.slice((messages.length-chosenValue),messages.length);
+		}
+
+		noOfMessages = event.target.value;
+	}
+
+	function toggleMode (event: any){
+		let chosenMode = event.target.id;
+
+		if(chosenMode=="dev"){
+			isDevOn = true;
+		}
+
+		else if(chosenMode == 'prod'){
+			isDevOn = false;
+			if(conn!=null){
+				conn.close();
+			}
+		}
+	}
 
 	function addMessage(message: messageType) {
 		if (
@@ -49,28 +84,29 @@
 		);
 	}
 
-	function dial() {
-		let conn: WebSocket;
+	function dial(conn: WebSocket) {
 		try {
+			if(!isDevOn)
 			conn = new WebSocket(`wss://${location.host}/subscribe`);
 		} catch (e) {
-			conn = new WebSocket(`ws://${location.host}/subscribe`);
+			conn = new WebSocket(`wss://${location.host}/subscribe`);
 		}
-
-		conn.addEventListener("close", (ev) => {
-			if (ev.code !== 1001) {
-				//appendLog("Reconnecting in 1s");
-				setTimeout(dial, 1000);
-			}
-		});
-		conn.addEventListener("open", () => {
-			console.info("websocket connected");
-		});
-		conn.addEventListener("message", (ev) => {
-			const obj = JSON.parse(ev.data);
-			console.log(obj);
-			addMessage(obj);
-		});
+		if(conn){
+			conn.addEventListener("close", (ev) => {
+				if (ev.code !== 1001) {
+					//appendLog("Reconnecting in 1s");
+					setTimeout(dial, 1000);
+				}
+			});
+			conn.addEventListener("open", () => {
+				console.info("websocket connected");
+			});
+			conn.addEventListener("message", (ev) => {
+				const obj = JSON.parse(ev.data);
+				console.log(obj);
+				addMessage(obj);
+			});
+		}
 		return true;
 	}
 
@@ -78,10 +114,31 @@
 		content = event.detail;
 	}
 
+	function configButtonHandler(){
+		var element = document.getElementById('configmodal');
+		var backdrop = document.getElementById('backdrop');
+
+		if(element.style.visibility == 'hidden'){
+			element.style.visibility = 'visible';
+			backdrop.style.visibility = 'visible';
+		}
+		else{
+			element.style.visibility = 'hidden';
+			backdrop.style.visibility = 'hidden';
+		}
+	}
+
+	function submitHandler(){
+		let backdrop = document.getElementById('backdrop');
+		let element = document.getElementById('configmodal');
+		backdrop.style.visibility = 'hidden';
+		element.style.visibility = 'hidden';
+	}
+
 	const sleep = (ms: number) => new Promise((f) => setTimeout(f, ms));
 
 	const test = async () => {
-		while (true) {
+		while(isDevOn){	
 			await sleep(1000);
 			addMessage({
 				action: "action",
@@ -99,8 +156,10 @@
 	};
 
 	onMount(() => {
-		dial();
-		// test();
+		// Default value of number of messages
+		noOfMessages = 50;
+		isDevOn = false;
+		conn = null;
 		validate();
 	});
 </script>
@@ -113,6 +172,22 @@
 	<input bind:value={filter} placeholder="Filter destination port" />
 	<button on:click={filterMessages}>Apply</button>
 	<span>Port number and '&&' to concat.</span>
+	<backdrop id="backdrop" on:click={submitHandler} style="visibility: hidden;"></backdrop>
+	<button id="configButton" on:click={configButtonHandler}>Config</button>
+		<div id="configmodal" style="visibility: hidden;">
+			<p>Number of messages</p>
+			<input type="number" min="0" on:change={defineMessages}>
+			<p>Mode</p>
+			<label>
+				<input type="radio" name="radio-group" id="dev" on:click={toggleMode}>
+				Development
+			</label>
+			<label>
+				<input type="radio" name="radio-group" checked id="prod" on:click={toggleMode}>
+				Production
+			</label>
+			<button on:click={submitHandler}>Apply</button>
+		</div>
 	{#if !isLoggedIn}
 		<SSOButton />
 	{:else}
@@ -123,30 +198,15 @@
 
 <main>
 	<div class="row">
-		<div
-			class="column"
-			id="message-log"
-			on:wheel={() => {
-				follow = false;
-			}}
-		>
+		<div class="column" id="message-log">
 			{#each messages as message (message.timestamp)}
 				{#if !filterPorts.includes(message.dstPort)}
-					<Message on:message={displayContent} {message} {follow} />
+					<Message on:message={displayContent} {message} />
 				{/if}
 			{/each}
 		</div>
 		<Content {content} />
 	</div>
-
-	{#if !follow}
-		<button
-			on:click={() => {
-				follow = true;
-			}}
-			id="resume-btn">Resume</button
-		>
-	{/if}
 </main>
 
 <style>
@@ -173,17 +233,32 @@
 		flex: 50%;
 		padding: 15px 20px;
 	}
+
 	#message-log {
 		width: 100%;
 		flex-grow: 1;
 		overflow-y: scroll;
 	}
 
-	#resume-btn {
+    #configmodal{
+        position: absolute;
+		z-index: 50;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
+		border: 1px black solid;
+		padding: 15px;
+		border-radius: 10px;
+		background-color: white;
+    }
+
+	#backdrop {
 		position: fixed;
-		bottom: 2rem;
-		z-index: 2;
-		left: 40vw;
-		cursor: pointer;
-	}
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100vh;
+		z-index: 20;
+		background-color: rgba(0, 0, 0, 0.75);
+  	}
 </style>
