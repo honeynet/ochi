085a3895fe96e0c36866d2701490994ddc3093cb
Kumiszhan Dybyspayeva <d.kumiszhan@gmail.com>
2023-06-13 00:38:19 +0100

    Do not use jest global functions but import them

--- src/__tests__/content.test.ts
+++ src/__tests__/content.test.ts
@@ -1,8 +1,9 @@
 import { render } from '@testing-library/svelte';
 import Content from '../Content.svelte';
+import { describe, expect, test } from '@jest/globals';
 
 describe('Content', () => {
-    test('render download button', () => {
+    test('renders download button', () => {
         const { getByText } = render(Content, { props: { content: { payload: 'test' } } });
         const node = getByText('Download');
         expect(node).not.toBeNull();
