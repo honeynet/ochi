ba57abf045e76c0c8fa2b7c09c9a78c215300886
Lukas Rist <glaslos@gmail.com>
2023-06-26 22:16:48 +0200

    adding  and  to the event

--- src/event.ts
+++ src/event.ts
@@ -1,12 +1,13 @@
 export interface Event {
     payload?: string;
-    action?: string;
     connKey?: number[];
     dstPort: number;
     rule?: string;
+    handler?: string;
     scanner?: string;
     sensorID: string;
     srcHost: string;
     srcPort: string;
     timestamp: string;
+    decoded?: any;
 }
