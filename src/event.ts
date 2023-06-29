988ca169f09d25df75197f91d854d4a31417b486
Lukas Rist <lukas@vivino.com>
2023-06-29 15:48:50 +0200

    event filed comments

--- src/event.ts
+++ src/event.ts
@@ -1,13 +1,13 @@
 export interface Event {
-    payload?: string;
-    connKey?: number[];
-    dstPort: number;
-    rule?: string;
-    handler?: string;
-    scanner?: string;
-    sensorID: string;
-    srcHost: string;
-    srcPort: string;
-    timestamp: string;
-    decoded?: any;
+    payload?: string; // base64 encoded binary payload
+    connKey?: number[]; // identifier based on IP and source port
+    dstPort: number; // the connection destination port
+    rule?: string; // the rule that matched the connection
+    handler?: string; // the processing handler
+    scanner?: string; // name of the scanner if detected
+    sensorID: string; // the id of the sensor
+    srcHost: string; // the source IP address
+    srcPort: string; // the source port
+    timestamp: string; // the UTC timestamp of the connection
+    decoded?: any; // a decoded version of the payload if available
 }
