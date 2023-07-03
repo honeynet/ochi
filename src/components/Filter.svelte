33bd6476173acd6a0b6f6cb8fd24791c3efd0259
Mashrukh Islam <mushroomgenie@Iceberg.local>
2023-07-03 12:30:29 +0530

    improve filter input

--- src/components/Filter.svelte
+++ src/components/Filter.svelte
@@ -6,11 +6,17 @@
 
     let filter: string = '';
     let filterValid: boolean = false;
+    let focus: boolean = false;
+
+    function handleFocus(): () => void {
+        focus = true;
+    }
 
     function filterChangeHandler(): () => void {
         return debounce(() => {
             // TODO: validate queries as user types them.
             console.log('filter is changing');
+            focus = true;
             let parseResult = parseDSL(filter);
             if (parseResult.lexErrors.length > 0 || parseResult.parseErrors.length > 0) {
                 console.log('Found some errors', parseResult.lexErrors, parseResult.parseErrors);
@@ -20,6 +26,10 @@
                 filterValid = true;
             }
         }, 1000);
+    }
+
+    function resetQuery() {
+        focus = false;
     }
 
     function applyFilter() {
@@ -45,11 +55,16 @@
 <section class="filter">
     <input
         class:input-error={filter && !filterValid}
+        class:input-focus={focus}
         bind:value={filter}
         placeholder="Filter destination port"
         on:input={filterChangeHandler()}
+        on:focus={handleFocus}
     />
     <Button disabled={!filterValid} onClick={applyFilter} text="Apply" />
+    {#if focus}
+        <Button onClick={resetQuery} text="Reset Query" />
+    {/if}
 </section>
 
 <style>
@@ -63,6 +78,10 @@
 
     input.input-error {
         border: 1px solid #ff0000;
+    }
+
+    input.input-focus {
+        width: 75%;
     }
 
     input.input-error:focus {
